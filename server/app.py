1d295d0c4fbb23d8765929fca266b615df4f21de
Newton Muhindi <developermuhindi@gmail.com>
2025-06-18 13:01:19 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,53 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods =['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_list = [message.to_dict() for message in messages]
+        return make_response(jsonify(messages_list), 200)
+    
+    elif request.method == 'POST':
+        try:
+            data = request.get_json()
+            new_message = Message(
+                body = data.get("body"),
+                username = data.get("username"),
+            )
+            db.session.add(new_message)
+            db.session.commit()
+            return make_response(jsonify(new_message.to_dict()), 201)
+        except Exception as e:
+            return make_response({"error": str(e)}, 400)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+    
+    if not message:
+        return make_response({
+            "message": "This message does not exist in our database."
+        }, 404)
+        
+    if request.method == 'GET':
+        return make_response(message.to_dict(), 200)
+    
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr (message, attr, data.get(attr))
+        db.session.add(message)
+        db.session.commit()
+        return make_response(message.to_dict(), 200)
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+    return make_response({
+        "delete_successful": True,
+        "message": "message deleted"
+    })
 
 if __name__ == '__main__':
     app.run(port=5555)
