cf7d8259bec56ebc777e94021674e909a5115c83
Newton Muhindi <developermuhindi@gmail.com>
2025-06-18 15:10:46 +0300

    updated complete lab

--- server/models.py
+++ server/models.py
@@ -8,11 +8,14 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+import datetime
+
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String)
     username = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
